672a6cb4e45c3ad4353f001181862f68d06a1cd4
Vikkanda <71092092+Vikkanda@users.noreply.github.com>
2020-09-20 12:26:36 -0700

    Update Q1.cpp

--- A1/Q1.cpp
+++ A1/Q1.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
+using namespace std;
+ 
 int main() {
 
-using namespace std;
     int Speed_variable (20);
     int Time_variable (10);
     int Distance_variable (Speed_variable*Time_variable);
     
     cout << "The total distance travelled is " << Distance_variable << endl;
-    
-    return 0;
+
+    return (0);
 }
