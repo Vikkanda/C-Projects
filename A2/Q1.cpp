ccf934e5f8a63524f3a51d39f0b0d6578f886a0c
Vikkanda <71092092+Vikkanda@users.noreply.github.com>
2020-09-26 09:42:08 -0700

    Create Q1.cpp

---
+++ A2/Q1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int selection;
+      cout << "Which formula do you want to see?\n\n";
+      cout << "1. Area of a circle\n";
+      cout << "2. Area of a rectangle\n";
+      cout << "3. Area of a cylinder\n";
+      cout << "4. None of them!\n\n";
+      cout << "Enter your selection from 1 - 4 below:\n\n"; 
+      
+      cin >> selection;
+ 
+switch (selection)
+    {
+        case 1:
+                cout << "\nPi times radius squared\n\n";
+                    break;
+        case 2:
+                cout << "\nLength times width\n\n";
+                    break;
+        case 3:
+                cout << "\nPi times radius squared times height\n\n";
+                    break;
+        case 4:
+                cout << "\nWell okay then, good bye!\n\n";
+                    break;
+        default:
+                cout << "\nNot good with numbers, eh?\n\n";
+    } 
+}
+
