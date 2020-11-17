0578da40e3acc17e1cacab8a28c2cbfbe24ecf2a
Vikkanda <71092092+Vikkanda@users.noreply.github.com>
2020-11-16 23:17:32 -0800

    Update Q8.cpp

--- A8/Q8.cpp
+++ A8/Q8.cpp
@@ -1,94 +1,105 @@
-# Bug 1
-
-1. **The incorrect original code or code snippit that you wrote:**
-
-``` cpp
-//code with bugs or code snippet with bug goes here
-
-//sample code
-
-// Online C++ compiler to run C++ online.
-// Write C++ code in this online editor and run it.
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+string name;
+string enrollmentYear;
+  
+public:
+  
+Student() {
+  
+}
+  
+Student(string name, string enrollmentYear) {
+this->name = name;
+this->enrollmentYear = enrollmentYear;
+}
+  
+string getName() {
+return name;
+}
+  
+string getEnrollmentYear() {
+return enrollmentYear;
+}
+  
+void setName(string name) {
+this->name = name;
+}
+  
+void setEnrollmentYear(string enrollmentYear) {
+this->enrollmentYear = enrollmentYear;
+}
+  
+virtual void print() {
+cout << "Name: " << name << endl;
+cout << "Enrollment Year: " << enrollmentYear << endl;
+}
+};
+
+class KpuStudent : public Student {
+private:
+string campusLocation;
+  
+public:
+  
+KpuStudent(string name, string enrollmentYear, string campusLocation) : Student(name, enrollmentYear) {
+this->campusLocation = campusLocation;
+}
+  
+string getCampusLocation() {
+return campusLocation;
+}
+  
+void setCampusLocation(string campusLocation) {
+this->campusLocation = campusLocation;
+}
+  
+virtual void print() {
+cout << "Name: " << getName() << endl;
+cout << "Campus Location: " << campusLocation << endl;
+}
+};
+class HogwartsStudent : public Student {
+private:
+string houseAffiliation;
+  
+public:
+HogwartsStudent(string name, string enrollmentYear, string houseAffiliation) : Student(name, enrollmentYear) {
+this->houseAffiliation = houseAffiliation;
+}
+  
+string getHouseAffiliation() {
+return houseAffiliation;
+}
+  
+void setHouseAffiliation(string houseAffiliation) {
+this->houseAffiliation = houseAffiliation;
+}
+  
+virtual void print() {
+cout << "Name: " << this->getName() << endl;
+cout << "House Affiliation: " << houseAffiliation << endl;
+}
+};
 
 int main() {
-    std:cout << "Hello World!";
   
-  return 0;
+Student student = Student("Vik Kanda", "2020");
+KpuStudent kpuStudent = KpuStudent("Vik Kanda", "2020", "Richmond");
+HogwartsStudent hogwartsStudent = HogwartsStudent("Vik Kanda", "1994", "Slytherin");
+  
+Student *students[3];
+  
+students[0] = &student;
+students[1] = &kpuStudent;
+students[2] = &hogwartsStudent;
+  
+for(int i = 0; i < 3; i++) {
+students[i]->print();
 }
-
-```
-
-2. **What bug does the original code have?**
-
-  
-
-3. **What misunderstanding of C++ concepts lead you to this incorrect code?**
-
-4. **How to correct the bug?**
-
-5. **The corresponding bug-free code or code snippet is:**
-
-```
-bug-free code or code snippet goes here
-
-```
-
-6. **What is the take-away message from this bug?**
-
----
-
-# Bug 2
-
-1. **The incorrect original code or code snippit that you wrote:**
-
-```
-code with bugs or code snippet with bug goes here
-
-```
-
-2. **What bug does the original code have?**
-
-  
-
-3. **What misunderstanding of C++ concepts lead you to this incorrect code?**
-
-4. **How to correct the bug?**
-
-5. **The corresponding bug-free code or code snippet is:**
-
-```
-bug-free code or code snippet goes here
-
-```
-
-6. **What is the take-away message from this bug?**
-
----
-
-# Bug 3
-
-1. **The incorrect original code or code snippit that you wrote:**
-
-```
-code with bugs or code snippet with bug goes here
-
-```
-
-2. **What bug does the original code have?**
-
-  
-
-3. **What misunderstanding of C++ concepts lead you to this incorrect code?**
-
-4. **How to correct the bug?**
-
-5. **The corresponding bug-free code or code snippet is:**
-
-```
-bug-free code or code snippet goes here
-
-```
-
-6. **What is the take-away message from this bug?**
+return 0;
+}
